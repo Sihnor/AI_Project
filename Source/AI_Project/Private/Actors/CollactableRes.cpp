f6e06a6de5a4dfb8eb7c2f3d08e3280fa539142b
Marcel <54372177+Sihnor@users.noreply.github.com>
2024-04-02 22:23:58 +0200

    Update CollactableRes.cpp

--- Source/AI_Project/Private/Actors/CollactableRes.cpp
+++ Source/AI_Project/Private/Actors/CollactableRes.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AI_Project\Public\Actors\CollectibleRes.h"
+#include "AI_Project/Public/Actors/CollectibleRes.h"
 
 
 // Add default functionality here for any ICollactableRes functions that are not pure virtual.
